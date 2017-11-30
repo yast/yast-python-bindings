b980522a1ab70d86f9c3e93e28c2b151bee12f6d
David Mulder <dmulder@suse.com>
2017-11-30 07:38:29 -0700

    Fix typo

--- src/yast.py
+++ src/yast.py
@@ -134,6 +134,7 @@ del scr_meta_funcs, scr_meta_func_creator, meta_func_creator, current_module, me
 def Term(*args):
     from ycp import Term as YCPTerm
     name = args[0]
+    l = None
     if len(args) > 1:
         l = List()
         for item in args[1:]:
