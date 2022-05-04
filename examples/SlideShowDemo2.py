c5563e4e9d8acffc534d6473b0f4a13fc409a4ed
Noel Power <noel.power@suse.com>
2022-05-04 16:25:30 +0100

    Wizard should use 'Symbol' instead of 'Id'
    To match Ruby examples e.g.
    
    Wizard(:back, "&back",.....) etc.
    
    we should use
    
    Wizard(Symbol("back", "&back",.....) etc. as the argument doesn't
    get automatically translated
    
    Signed-off-by: Noel Power <noel.power@suse.com>

--- examples/SlideShowDemo2.py
+++ examples/SlideShowDemo2.py
@@ -63,11 +63,11 @@ class SlideShowDemo2Client:
         Opt("defaultsize"),
         Wizard(
           Opt("stepsEnabled"),
-          Id("back"),
+          Symbol("back"),
           "&Back",
-          Id("abort"),
+          Symbol("abort"),
           "Ab&ort",
-          Id("next"),
+          Symbol("next"),
           "&Next"
         )
       )
